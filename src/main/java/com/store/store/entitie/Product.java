c3fb4666f3d14f674917ea8b7e621ef59ce3ed4e
Diego Villalba <diego.villalba01@uptc.edu.co>
2024-03-23 09:27:56 -0500

    conexcion DB y entidad productos

---
+++ src/main/java/com/store/store/entitie/Product.java
@@ -0,0 +1,40 @@
+package com.store.store.entitie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+
+@Entity
+@Data
+@Table(name = "Product")
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "img1")
+    private String img1;
+
+    @Column(name = "img2")
+    private String img2;
+
+    @Column(name = "img3")
+    private String img3;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category")
+    private String category;
+
+}
