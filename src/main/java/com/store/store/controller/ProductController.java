2a3ccb9d64c1a2a17ec18cff9df75275eabae7a3
Diego Villalba <diego.villalba01@uptc.edu.co>
2024-03-23 10:24:06 -0500

    pagina productos

--- src/main/java/com/store/store/controller/ProductController.java
+++ src/main/java/com/store/store/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.store.store.controller;
 import com.store.store.entitie.Product;
 import com.store.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +18,12 @@ public class ProductController {
     @PostMapping("/add")
     public Product add(@RequestBody Product product){
         return productService.add(product);
+    }
+
+    @GetMapping("/list")
+    public Page<Product>list(@RequestParam String name, @RequestParam int page, @RequestParam int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return productService.list(name, pageable);
     }
 }
 
--- src/main/java/com/store/store/service/ProductService.java
+++ src/main/java/com/store/store/service/ProductService.java
@@ -3,8 +3,9 @@ package com.store.store.service;
 import com.store.store.entitie.Product;
 import com.store.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class ProductService {
@@ -12,8 +13,11 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping
     public Product add(Product product) {
         return productRepository.save(product);
+    }
+
+    public Page<Product> list(String name, Pageable pageable){
+        return productRepository.findByNameContainingIgnoreCase(name, pageable);
     }
 }
